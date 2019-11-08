d6ef20062ded090d0080e65ac06acd1e22cd48d0
aleesha-wood <spectrat@spectratart.com>
2019-11-08 05:00:21 -0500

    button e-handler bases with expected explainations built out, tests started

--- baseball-scoreboard/src/components/Dashboard.js
+++ baseball-scoreboard/src/components/Dashboard.js
@@ -4,28 +4,40 @@ import Display from './Display';
 class Dashboard extends React.Component {
 
   state = {
-    balls: 0,
-    strikes: 0
+    ball: 0,
+    strike: 0
+  };
+
+  //Button event handlers
+  hitHandler = event => {
+    //sets ball and strike state to 0
+  };
+  ballHandler = event => {
+    //adds +1 to ball state, unless it's at 3, then strike and ball states are set to O
+  };
+  foulHandler = event => {
+    //adds +1 to strike state, unless it's at 2, then it stays at 2.
+  };
+  strikeHandler = event => {
+    //adds +1 to strike state, unless it's at 2, then strike and ball state are set to 0
   };
 
   render() {
-
-
     return (
-    <div className="container">
+      <div className="container">
 
-      <Display />
+        <Display />
 
-      Square div that shows display.
+        Square div that shows display.
 
-      <div className="buttons">
-        <button>Hit</button>
-        <button>Ball</button>
-        <button>Foul</button>
-        <button>Strike</button>
+        <div className="buttons">
+          <button>Hit</button>
+          <button>Ball</button>
+          <button>Foul</button>
+          <button>Strike</button>
+        </div>
       </div>
-    </div>
-  )};
+    )};
 };
 
 export default Dashboard;
--- baseball-scoreboard/src/components/Dashboard.test.js
+++ baseball-scoreboard/src/components/Dashboard.test.js
@@ -6,3 +6,15 @@ import Dashboard from './Dashboard';
 test('renders display component', () => {
   rtl.render(<Dashboard />);
 });
+
+test('hitHandler: adds sets both ball and strike state to 0', () => {
+  expect(this.state.ball).toBe(0);
+  expect(this.state.strike).toBe(0);
+});
+
+test("ballHandler: adds +1 to ball state, unless it's at 3, then strike and ball states are set to O", () => {
+  expect(this.state.ball).toBe(this.state.ball+1);
+  expect(this.state.ball).not.toBe(4);
+})
+//I don't think this will accurately reflect the event handler.
+//TODO go through the documentation and look for simulation tips...Also this stackoverflow link https://stackoverflow.com/questions/42984108/testing-react-event-handlers 

d60cbc2cbc85272538963fffcf6deb613695b1a7
aleesha-wood <spectrat@spectratart.com>
2019-11-08 02:27:40 -0500

    added component test files

--- baseball-scoreboard/src/components/Display.js
+++ baseball-scoreboard/src/components/Display.js
@@ -1,1 +1,11 @@
 import React from 'react';
+
+const Display = () => {
+  return (
+    <div className="container">
+      Square Container and Dashboard stuff goes here!
+    </div>
+  )
+}
+
+export default Display;

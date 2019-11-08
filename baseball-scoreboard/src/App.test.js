dc6d2406510239fa72773d42bf5ec72e27ec8347
aleesha-wood <spectrat@spectratart.com>
2019-11-08 00:42:33 -0500

    app mount test formatted for new libs

--- baseball-scoreboard/src/App.test.js
+++ baseball-scoreboard/src/App.test.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import * as rtl from '@testing-library/jest-dom/extend-expect';
+import * as rtl from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+test('render without crashing', () => {
+  rtl.render(<App />);
 });
